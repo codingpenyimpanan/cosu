9cb06809331efb6cdc6957aa49e60ddf20d0f5e5
root <root@LAPTOP-9EFHH6T4>
2023-02-12 14:40:26 +0700

    belajar python sederhana

---
+++ P1.py
@@ -0,0 +1,14 @@
+a = (input("Input Nama      : "))
+b = (input("Input Sekolah   : "))
+c = (int(input("Input Umur      : ")))
+d = (float(input("Input BB        : ")))
+
+if b == "SMA":
+    e = True
+else:
+    e = False
+print("============================\n")
+print(f"Nama               : {a}")
+print(f"Sekolah            : {e}")
+print(f"Umur               : {c}")
+print(f"BB                 : {d}")
